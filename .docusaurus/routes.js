e4bbd82d29167f62f1e58dbc124a5f45529cc64c
Ryan Lehmann <rlehmann@kameleoon.com>
2024-03-06 16:29:03 -0500

    test

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug/',
+    component: ComponentCreator('/__docusaurus/debug/', '600'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config/',
+    component: ComponentCreator('/__docusaurus/debug/config/', '08f'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content/',
+    component: ComponentCreator('/__docusaurus/debug/content/', 'a2a'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData/',
+    component: ComponentCreator('/__docusaurus/debug/globalData/', 'ea5'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata/',
+    component: ComponentCreator('/__docusaurus/debug/metadata/', '05b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry/',
+    component: ComponentCreator('/__docusaurus/debug/registry/', 'dab'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes/',
+    component: ComponentCreator('/__docusaurus/debug/routes/', '253'),
+    exact: true
+  },
+  {
     path: '/docs/',
     component: ComponentCreator('/docs/', '8ec'),
     routes: [
