969a368a1a979195aa7f44213c4bddab604868a4
Ryan Lehmann <rlehmann@kameleoon.com>
2024-03-06 16:42:35 -0500

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'r-lehmann', // Usually your GitHub org/user name.
-  projectName: 'about', // Usually your repo name.
+  projectName: 'r-lehmann.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
